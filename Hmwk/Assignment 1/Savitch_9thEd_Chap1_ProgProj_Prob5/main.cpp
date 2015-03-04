d0173e94d287d2cdaddf1c4f0d080693e8772a9c
caitlinswafford <caitlinswafford@gmail.com>
2015-03-04 11:12:20 -0800

    Savitch_9thEd_Chap1_ProgProj_Prob5

---
+++ Hmwk/Assignment 1/Savitch_9thEd_Chap1_ProgProj_Prob5/main.cpp
@@ -0,0 +1,37 @@
+/* 
+ * File:   main.cpp
+ * Author: caitlinswafford
+ *
+ * Created on March 4, 2015, 9:31 AM    
+ *      Purpose: To create Big C out of X's
+ */
+
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!!
+
+int main(int argc, char** argv) {
+ //Declare Variables
+    char x='x';
+    //Output Big C
+    cout<<"    "<<x<<x<<x<<x<<endl;
+    cout<<"  "<<x<<"    "<<x<<endl;
+    cout<<" "<<x<<"      "<<endl;
+    cout<<x<<endl;
+    cout<<x<<endl;
+    cout<<x<<endl;
+    cout<<x<<endl;
+    cout<<" "<<x<<"      "<<endl;
+    cout<<"  "<<x<<"    "<<x<<endl;
+    cout<<"    "<<x<<x<<x<<x<<endl;
+    return 0;
+}
+
