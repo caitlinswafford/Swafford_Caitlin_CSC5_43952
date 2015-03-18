01d29a11ada0beba88a6755dc3f8a4c49934f8d8
caitlinswafford <caitlinswafford@gmail.com>
2015-03-18 10:40:30 -0700

    PayCheckTernary

---
+++ Lab/PayCheck If Else/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=paycheck_if_else
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/paycheck_if_else
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=paycheckifelse.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/paycheckifelse.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=paycheck_if_else
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/paycheck_if_else
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=paycheckifelse.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/paycheckifelse.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
---
+++ Lab/PayCheckIfStatements/main.cpp
@@ -0,0 +1,65 @@
+/* 
+ * File:   main.cpp
+ * Author: caitlinswafford
+ *
+ * Created on March 16, 2015, 8:51 AM
+ *      Purpose: Paycheck-If Else
+ */
+
+//System Libraries
+#include <iostream>//I/O
+#include <iomanip>//Formatting
+#include <fstream>>//I/O File
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution starts here!
+int main(int argc, char** argv) {
+ //Declare Variables
+    ofstream out;//Output the results in a file
+    float hrsWrkd;//Hours Worked (hrs)
+    float payRate;// Pay Rate ($'s/hr)
+    float oRate=1.5f;//Multiplicative overtime of PayRate
+    float oTime=40.0f;//Where overtime begins (hr)
+    float payChk;//Gross Pay ($'s)
+    //Open File
+    out.open("Payroll.dat");
+    //Prompt the user for inputs
+    cout<<"How many hours this week did you work?"<<endl;
+    cin>>hrsWrkd;
+    cout<<"What is your pay rate ($'s/hr)"<<endl;
+    cin>>payRate;
+    //Calculate the PayCheck
+    if(hrsWrkd<oTime){
+        payChk=payRate*hrsWrkd;
+    }
+    if(hrsWrkd>=oTime){
+        payChk=payRate*(hrsWrkd+(oRate-1)*(hrsWrkd-oTime));
+}
+    //Output the results to the screen
+    cout<<fixed<<setprecision(2)<<showpoint;
+    cout<<"Hours Worked = "<<hrsWrkd<<"(hrs)"<<endl;
+    cout<<"Pay Rate = $"<<payRate<<"(hr)"<<endl;
+    cout<<"Over time rate = "<<oRate<<endl;
+    cout<<"Over time begins at "<<oTime<<"(hrs)"<<endl;
+    cout<<"Gross Pay = $ "<<payChk<<endl;
+    //Close File
+    out.close();  
+    //Output the results to the file
+    out<<fixed<<setprecision(2)<<showpoint;
+    out<<fixed<<setprecision(2)<<showpoint;
+    out<<"Hours Worked = "<<hrsWrkd<<"(hrs)"<<endl;
+    out<<"Pay Rate = $"<<payRate<<"(hr)"<<endl;
+    out<<"Over time rate = "<<oRate<<endl;
+    out<<"Over time begins at "<<oTime<<"(hrs)"<<endl;
+    out<<"Gross Pay = $ "<<payChk<<endl;
+    //Close File
+    out.close();  
+    return 0;
+}
+
